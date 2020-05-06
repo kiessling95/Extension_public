60738ebc2125abc29c954626ca720eb35ce7db6c
german <gkiessling_95@hotmail.com>
2020-05-06 11:50:12 -0300

    Models

---
+++ views/destinatarios/index.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Destinatarios');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="destinatarios-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Destinatarios'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id_destinatario',
+            'domicilio',
+            'telefono',
+            'email:email',
+            'contacto',
+            //'id_pext',
+            //'descripcion',
+            //'id_localidad',
+            //'id_provincia',
+            //'id_pais',
+            //'cantidad',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
